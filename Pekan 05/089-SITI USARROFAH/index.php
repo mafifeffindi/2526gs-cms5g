80b1ba1b23a1441ae0cd54d6245de19bf813a988
rofaaa089 <musarrofasiti928@gmail.com>
2025-09-18 08:19:33 +0700

    Add files via upload

--- Pekan 05/089-SITI USARROFAH/index.php
+++ Pekan 05/089-SITI USARROFAH/index.php
@@ -1,20 +1,14 @@
 <?php get_header(); ?>
 
-<?php if (have_posts()) : ?>
-    <?php while (have_posts()) : the_post(); ?>
-        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-            <div class="post-meta">
-                <small>Diposting pada <?php the_time('d M Y'); ?> oleh <?php the_author(); ?></small>
-            </div>
-            <div class="post-content">
-                <?php the_excerpt(); ?>
-            </div>
-        </article>
-        <hr>
-    <?php endwhile; ?>
+<?php if ( have_posts() ) : ?>
+  <?php while ( have_posts() ) : the_post(); ?>
+    <article>
+      <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+      <div><?php the_excerpt(); ?></div>
+    </article>
+  <?php endwhile; ?>
 <?php else : ?>
-    <p>Tidak ada posting ditemukan.</p>
+  <p>Tidak ada posting ditemukan.</p>
 <?php endif; ?>
 
 <?php get_footer(); ?>
