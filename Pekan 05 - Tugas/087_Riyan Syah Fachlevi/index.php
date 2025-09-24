88eae3865fb1f3af8c3e57cd323da5131d67a1e5
Riyanfachlevi123 <riyanfachlevi@gmail.com>
2025-09-24 21:59:06 +0700

    Update index.php

--- Pekan 05 - Tugas/087_Riyan Syah Fachlevi/index.php
+++ Pekan 05 - Tugas/087_Riyan Syah Fachlevi/index.php
@@ -1,1 +1,19 @@
+<?php get_header(); ?>
 
+<div class="container">
+    <main>
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <article>
+                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                <?php if (has_post_thumbnail()) the_post_thumbnail('medium'); ?>
+                <div><?php the_excerpt(); ?></div>
+            </article>
+        <?php endwhile; else : ?>
+            <p>Postingan tidak ditemukan.</p>
+        <?php endif; ?>
+    </main>
+
+    <?php get_sidebar(); ?>
+</div>
+
+<?php get_footer(); ?>
