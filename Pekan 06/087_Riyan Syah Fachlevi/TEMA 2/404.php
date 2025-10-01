76eed94fc1bbec12022b6fd6e026e6f7477ab20c
Riyanfachlevi123 <riyanfachlevi@gmail.com>
2025-10-01 21:20:27 +0700

    Update 404.php

---
+++ Pekan 06/087_Riyan Syah Fachlevi/TEMA 2/404.php
@@ -0,0 +1,6 @@
+<?php get_header(); ?>
+<article>
+  <h1>Oops! Halaman tidak ditemukan 💛</h1>
+  <p>Coba kembali ke <a href="<?php echo home_url(); ?>">beranda</a>.</p>
+</article>
+<?php get_footer(); ?>
