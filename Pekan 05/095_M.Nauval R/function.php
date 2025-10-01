e9cc07a7e54d6e5b6928cc78ca0c5dd4ec8ba246
MNauvalRamadhani <kaconkfernandes@gmail.com>
2025-10-01 08:22:14 -0700

    Add files via upload

---
+++ Pekan 05/095_M.Nauval R/function.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Mendaftarkan stylesheet utama tema.
+ */
+function tema_saya_register_styles() {
+    // get_stylesheet_uri() akan otomatis mengambil URL dari file style.css di root tema.
+    wp_enqueue_style( 'tema-saya-style', get_stylesheet_uri() );
+}
+
+// Menjalankan fungsi di atas saat WordPress memuat script dan style
+add_action( 'wp_enqueue_scripts', 'tema_saya_register_styles' );
+
+?>  
