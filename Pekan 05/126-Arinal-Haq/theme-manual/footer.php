490202ee90b3ccbe0ea6e2416b55d9d5cc6bb4f6
ArinalHaq89 <arinalafasy@gmail.com>
2025-12-03 14:43:05 +0700

    Delete Pekan 05/126-Arinal-Haq directory

--- Pekan 05/126-Arinal-Haq/theme-manual/footer.php
+++
@@ -1,7 +0,0 @@
-</div> <footer>
-    <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</p>
-</footer>
-
-<?php wp_footer(); /* Hook wajib WP untuk admin bar & script */ ?>
-</body>
-</html>
