17f38d3bb4ca1c6e094f3319047b6152f34f9ce0
iqbalfahadh23 <iqbalfahadh69@gmail.com>
2025-12-16 14:02:00 +0700

    Create index.php

---
+++ Pekan 06/091_Afif Makki/tugas2/index.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+
+<div class="hero">
+    <img src="https://th.bing.com/th/id/OIP.k7dvkQVKT4ioP-SqoK7HEQHaHa?w=192&h=192&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3" alt="Foto Mahasiswa Informatika">
+
+    <div class="hero-text">
+        <h2>Selamat Datang di Website Afif Makki 091</h2>
+        <p>Menyediakan jurnal, artikel, dan materi pembelajaran seputar dunia teknologi informatika dan pemrograman.</p>
+    </div>
+</div>
+
+<section>
+    <h2>Artikel Terbaru</h2>
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <article>
+            <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+            <p><?php the_excerpt(); ?></p>
+        </article>
+    <?php endwhile; else: ?>
+        <p>Belum ada artikel.</p>
+    <?php endif; ?>
+</section>
+
+<?php get_footer(); ?>
