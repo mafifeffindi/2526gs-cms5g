d5bc6aa8905605d6550cb73c06abc42c485fd8d2
Inisiapayaa <86315204+Inisiapayaa@users.noreply.github.com>
2026-01-03 20:24:56 +0700

    Create header.php

---
+++ Pekan 05/085_Fari Iqbal/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?php bloginfo('name'); ?></title>
+    <?php wp_head(); ?>
+</head>
+<body>
+
+<header>
+    <h2><?php bloginfo('name'); ?></h2>
+</header>
